db2778bfea0c70e1f523b2487fd601909d36a710
mxinseo <kyungminseo10@gmail.com>
2026-01-04 02:01:19 +0900

    Fix : docs/quest API 년도 2026으로 수정

--- src/api/apiModule.jsx
+++ src/api/apiModule.jsx
@@ -128,7 +128,7 @@ const apiModule = {
 
   fetchQuestions: async () => {
     const url = "/api/manage/docs/quest";
-    const year = "2025";
+    const year = "2026";
     const tracks = ["common", "pm", "fe", "be"];
 
     try {
